4e9d5544f9659790f7945786cc4f529ed2aa9ca0
albre <amc2kmol@gmail.com>
2025-04-26 19:53:26 -0700

    Update app.py for Render hosting

--- app.py
+++ app.py
@@ -98,5 +98,10 @@ def logout():
     return redirect(url_for('badge_scan'))
 
 
+import os
+
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
     app.run(debug=True)
